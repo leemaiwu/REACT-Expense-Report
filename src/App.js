c7413fde75fd9ebdb049e4aa7a9962d376dbe3ae
Laura Wu <leemaiwu@gmail.com>
2023-04-11 14:26:08 -0600

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import Expenses from './components/Expenses'
+
 function App() {
-  return (
+  const expenses = [
+    {
+      id: 'e1', 
+    title: 'Toilet Paper', 
+    amount: 94.12, 
+    date: new Date(2023, 1, 8)
+    },
+    {
+      id: 'e2', 
+      title: 'Car Insurance', 
+      amount: 295.67, 
+      date: new Date(2023, 3, 28)
+    },
+    {
+      id: 'e3', 
+      title: 'New TV', 
+      amount: 899.49, 
+      date: new Date(2023, 2, 10)
+    },
+    {
+      id: 'e4', 
+      title: 'New Desk (Wood)', 
+      amount: 450, 
+      date: new Date(2023, 4, 11)
+    }
+  ]
+
+  return(
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
     </div>
-  );
+  )
 }
 
 export default App;
