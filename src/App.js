68af38e2dbd79e8bd0e371389153ce6a18aff4b9
Laura Wu <leemaiwu@gmail.com>
2023-04-13 17:17:37 -0600

    Filter feature works

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import NewExpense from './components/NewExpenses/NewExpense'
 import Expenses from './components/Expenses/Expenses'
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1', 
+  title: 'Toilet Paper', 
+  amount: 94.12, 
+  date: new Date(2023, 1, 8)
+  },
+  {
+    id: 'e2', 
+    title: 'Car Insurance', 
+    amount: 295.67, 
+    date: new Date(2023, 3, 28)
+  },
+  {
+    id: 'e3', 
+    title: 'New TV', 
+    amount: 899.49, 
+    date: new Date(2023, 2, 10)
+  },
+  {
+    id: 'e4', 
+    title: 'New Desk (Wood)', 
+    amount: 450, 
+    date: new Date(2023, 4, 11)
+  }
+]
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1', 
-    title: 'Toilet Paper', 
-    amount: 94.12, 
-    date: new Date(2023, 1, 8)
-    },
-    {
-      id: 'e2', 
-      title: 'Car Insurance', 
-      amount: 295.67, 
-      date: new Date(2023, 3, 28)
-    },
-    {
-      id: 'e3', 
-      title: 'New TV', 
-      amount: 899.49, 
-      date: new Date(2023, 2, 10)
-    },
-    {
-      id: 'e4', 
-      title: 'New Desk (Wood)', 
-      amount: 450, 
-      date: new Date(2023, 4, 11)
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
   }
 
   return(
